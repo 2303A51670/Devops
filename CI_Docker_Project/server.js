78deea0a75d3ff9bc9b5f47dcbaa0bca73e7260c
Samshray <2303A51670@sru.edu.in>
2026-03-10 12:27:35 +0530

    Initial commit

---
+++ CI_Docker_Project/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Personal Finance Tracker API');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
